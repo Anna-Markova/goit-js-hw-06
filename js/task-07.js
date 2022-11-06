8be1ff1dff7e137606d31b86ef03d89f91815eed
Anna-Markova <yavdokimenkoanna88@gmail.com>
2022-11-06 07:27:41 +0200

    Update task-07.js
    сохранила изменения работает скрол на увеличение уменьшение шрифта

--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,9 @@
 const input = document.querySelector("#font-size-control");
 const output = document.querySelector("#text");
 output.style.fontSize = input.value+"px";
+const inputTextRef = document.querySelector("#text");
+const inputControlRef = document.querySelector("#font-size-control");
+inputControlRef.oninput = function () {
+    console.log(this.value)
+    inputTextRef.style.fontSize = inputControlRef.value + 'px';
+}
